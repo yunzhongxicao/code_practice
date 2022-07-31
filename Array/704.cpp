9dc947cff7125be359cbc1bac9d23e4d569832b9
dfc <yunzhongxicao@sjtu.edu.cn>
2022-08-01 00:17:21 +0800

    完成左闭右闭的二分法写法

--- Array/704.cpp
+++ Array/704.cpp
@@ -7,17 +7,18 @@
 #include "iostream"
 #include "vector"
 using namespace std;
-int search(vector<int>&nums, int target);
+int search_1(vector<int>&nums, int target);
+int search_2(vector<int>&nums, int target);
 
 
 int main()
 {
     vector<int> a = {-1,0,3,5,9,12};
-    int  target = 2;
-    search(a,target);
+    int  target = 8;
+    search_2(a,target);
 }
 
-int search(vector<int>&nums, int target)
+int search_1(vector<int>&nums, int target)  // 这里是没有用到二分法的
 {
     int num_length = nums.size();
     for (int i = 0; i < num_length; ++i) {
@@ -33,3 +34,30 @@ int search(vector<int>&nums, int target)
     }
 
 }
+
+int search_2(vector<int>&nums, int target)  //这个是左闭右闭的二分法写法
+{
+    int left =  0;
+    int right = nums.size()-1;
+    while (left<=right)
+    {
+        int middle = (int ) (left+right)/2;
+        if (nums[middle] < target)
+        {
+            left = middle+1;
+        }
+        else if (nums[middle]>target)
+        {
+            right = middle-1;
+        } else if (nums[middle] == target)
+        {
+            cout<<middle;
+            return middle;
+            break;
+        }
+    }
+    cout<<-1;
+    return -1;
+
+}
+
