64d73ab30abefbd088318916ae277233bec68f26
dfc <yunzhongxicao@sjtu.edu.cn>
2022-08-01 14:38:10 +0800

    35题的二分写法

--- Array/35.cpp
+++ Array/35.cpp
@@ -8,4 +8,56 @@
 #include "vector"
 using namespace std;
 
+int searchInsert_1(vector<int>& nums, int target);
+int searchInsert_2(vector<int>& nums, int target);
+int main()
+{
+    vector<int> a = {1,3,5,6};
+    int  target = 8;
+    searchInsert_2(a,target);
+    return 0;
+}
+
+int searchInsert_1(vector<int>& nums, int target)  //先通过暴力的方式进行
+{
+    for (int i = 0; i < nums.size(); ++i) {
+        if (nums[i]>=target)
+        {
+            cout<<i;
+            return i;
+        }
+    }
+    cout<< nums.size();
+    return nums.size();
+}
+
+
+int searchInsert_2(vector<int>& nums, int target)  //尝试利用二分的方法
+{
+    int left = 0;
+    int right = nums.size()-1;
+    int middle = 0;
+    while (left<=right)
+    {
+        middle = (int )(left+right)/2;
+        if (nums[middle] > target)
+        {
+            right = middle-1;
+        }
+        else if(nums[middle]<target)
+        {
+            left = middle +1;
+        }
+        else if (nums[middle] == target)
+        {
+            cout<<middle;
+            return middle;
+        }
+    }
+    cout<<left;
+    return left;
+}
+
+
+
 
