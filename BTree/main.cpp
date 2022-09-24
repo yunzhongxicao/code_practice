21a68fdaf3e6954f0d6b037df3ab465fa0886084
dfc <yunzhongxicao@sjtu.edu.cn>
2022-09-24 12:43:39 +0800

    二叉树学习

---
+++ BTree/main.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include "queue"
+
+using namespace std;
+
+//BTree类的前向说明
+template <class elemType>
+class BTree;
+
+
+template <class elemType>
+class Node   // node是每个节点类
+{
+    friend class BTree<elemType>;
+private:
+    elemType data;
+    Node *left, *right;
+    int leftFlag; //用于标识是否线索，0时left为左孩子结点，1时为前驱线索
+    int rightFlag; //用于标识是否线索，0时right为右孩子结点，1时为后继线索
+public:
+    Node(){left=NULL; right=NULL; leftFlag= 0; rightFlag=0;};
+    Node(const elemType&e, Node* L=NULL, Node *R=NULL)
+    { data = e;
+        left=L; right=R; leftFlag= 0; rightFlag=0;
+    };
+};
+
+
+
+template <class elemType>
+class BTree
+{
+private:
+    Node<elemType> *root;
+    elemType stopFlag;
+
+    int Size (Node<elemType> *t); //求以t为根的二叉树的结点个数。
+    int Height (Node<elemType> *t); //求以t为根的二叉树的高度。
+    void DelTree(Node<elemType> *t);//删除以t为根的二叉树
+    void PreOrder(Node<elemType> *t);
+    // 按前序遍历输出以t为根的二叉树的结点的数据值
+    void InOrder(Node<elemType> *t);
+    // 按中序遍历输出以t为根的二叉树的结点的数据值
+    void PostOrder(Node<elemType> *t);
+    // 按后序遍历输出以t为根的二叉树的结点的数据值
+public:
+    BTree(){root=NULL;}  // 构造函数 使得root指向空
+    void createTree(const elemType &flag);//创建一棵二叉树
+    bool isEmpty () { return (root == NULL);}// 判断叉树是否为空
+    Node<elemType> * GetRoot(){ return  root; }
+
+    int Size (); //求二叉树的结点个数。
+    int Height (); //求二叉树的高度。
+    void DelTree();//删除二叉树
+    void PreOrder();// 按前序遍历输出二叉树的结点的数据值
+    void InOrder();// 按中序遍历输出二叉树的结点的数据值
+    void PostOrder();// 按后序遍历输出二叉树的结点的数据值
+    void LevelOrder();// 按层次遍历输出二叉树的结点的数据值
+};
+
+template <class elemType>
+void BTree<elemType>::createTree(const elemType &flag) {
+    Node<elemType>  *p,*pl,*pr;
+    elemType x,xl,xr;
+
+    queue<Node<elemType>*> my_queue;
+    
+    cout<<"please input the root: ";
+    cin>>x;
+
+    if (x == flag)
+    {
+        root = NULL;
+        return;
+    }
+    
+    p = new Node<elemType>(x);
+
+    root = p;
+    my_queue.push(root);
+
+    while (!my_queue.empty())
+    {
+        p = my_queue.front();
+        my_queue.pop();
+        cout<<"please input left and right of "<<p->data;
+        cin>>xl>>xr;
+
+        if (xl != flag)
+        {
+            pl= new Node<elemType>(xl);
+            my_queue.push(pl);
+            p->left = pl;
+        }
+        if(xr != flag)
+        {
+            pr = new Node<elemType>(xr);
+            p ->right = pr;
+            my_queue.push(pr);
+        }
+
+    }
+}
+
+template <class elemType>
+void BTree<elemType>::LevelOrder() {
+    queue<Node<elemType>*> my_queue;
+    Node<elemType> *p,*pl,*pr;
+    if (root==NULL)
+    {
+        return;
+    }
+    cout<<root->data<<" ";
+    my_queue.push(root);
+    while (!my_queue.empty())
+    {
+        p = my_queue.front();
+        my_queue.pop();
+        if (p->left != NULL)
+        {
+            my_queue.push(p->left);
+            cout<<p->left->data<<" ";
+        }
+        if(p->right != NULL)
+        {
+            my_queue.push(p->right);
+            cout<<p->right->data<<" ";
+        }
+    }
+}
+
+
+template <class elemType>
+void BTree<elemType>::PreOrder() {
+    PreOrder(root);
+}
+
+template <class elemType>
+void BTree<elemType>::PreOrder(Node<elemType> *t) {
+    if (t== NULL)
+        return;
+    cout<<t->data;
+    PreOrder(t->left);
+    PreOrder(t->right);
+}
+
+template <class elemType>
+void BTree<elemType>::InOrder() {
+    InOrder(root)   ;
+}
+
+template <class elemType>
+void BTree<elemType>::InOrder(Node<elemType> *t) {
+    if (t==NULL)
+        return;
+    InOrder(t->left);
+    cout<<t->data;
+    InOrder(t->right);
+}
+
+
+int main()
+{
+    BTree<char> tree_1;
+    char flag = '&';
+    tree_1.createTree(flag);
+    tree_1.LevelOrder();
+}
+
