b81aade403b0d8a3caa941227277b00147b9c843
dfc <yunzhongxicao@sjtu.edu.cn>
2022-10-31 11:51:28 +0800

    406根据身高重新插入队列过渡

---
+++ Greedy/406.cpp
@@ -0,0 +1,17 @@
+/*
+@File  :406.cpp
+@Author:dfc
+@Date  :2022/10/31  11:47
+@Desc  :根据身高重建队列
+*/
+#include "vector"
+#include "iostream"
+using namespace std;
+
+bool static cmp(const vector<int> &a, const vector<int> &b)
+{
+    
+}
+
+
+vector<vector<int>> reconstructQueue(vector<vector<int>>& people);
