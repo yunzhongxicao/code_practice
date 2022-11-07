04f920759909cd3af04b94307348734dc44929be
dfc <yunzhongxicao@sjtu.edu.cn>
2022-11-07 15:36:17 +0800

    2维背包

--- Dynamic/bag.cpp
+++ Dynamic/bag.cpp
@@ -11,6 +11,31 @@ using namespace std;
 
 void test_2_wei_bag_problem()
 {
-    
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+
+    // ≥ı ºªØ
+    vector<vector<int>> dp(weight.size(),vector<int>(bagWeight+1,0));
+
+    for (int j = 0; j <=bagWeight ; ++j) {
+        if (j>=weight[0]) dp[0][j]=value[0];
+    }
+
+    for (int i = 1; i <=weight.size()-1 ; ++i) {
+        for (int j = 1; j <=bagWeight ; ++j) {
+            if (j<weight[i]) dp[i][j] = dp[i-1][j];
+            else
+            {
+                dp[i][j] = max(dp[i-1][j],value[i]+dp[i-1][j-weight[i]]);
+            }
+        }
+    }
+    cout << dp[weight.size() - 1][bagWeight] << endl;
+}
+
+int main()
+{
+    test_2_wei_bag_problem();
 }
 
