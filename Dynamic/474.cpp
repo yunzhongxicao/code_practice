391736d5a8844893812e57daa455cfa7a57da995
dfc <yunzhongxicao@sjtu.edu.cn>
2022-11-09 13:14:19 +0800

    474 一和零的二维写法

--- Dynamic/474.cpp
+++ Dynamic/474.cpp
@@ -20,7 +20,7 @@ vector<int> num_zero(const string &s)
     return vector<int>{num0,num1};
 }
 
-int findMaxForm(vector<string>& strs, int m, int n)
+/*int findMaxForm(vector<string>& strs, int m, int n)  三维写法
 {
     int length = strs.size();
     vector<vector<vector<int>>> dp(length,vector<vector<int>>(m+1,vector<int>(n+1,0)));
@@ -54,8 +54,34 @@ int findMaxForm(vector<string>& strs, int m, int n)
         }
     }
     return dp[length-1][m][n];
-}
+}*/
 
+int findMaxForm(vector<string>& strs, int m, int n) // 二维写法
+{
+    vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+    vector<int> tmp = num_zero(strs[0]);
+    int tmp0 = tmp[0];
+    int tmp1 = tmp[1];
+
+    for (int j =tmp0 ; j <=m ; ++j) {
+        for (int k = tmp1; k <=n ; ++k) {
+            dp[j][k] =1;
+        }
+    }
+
+    for (int i = 1; i <strs.size() ; ++i) {
+        tmp = num_zero(strs[i]);  // 这个计算放到后面两个for的前面 对每一层来说 0 和1的数量是确定的
+        // 亲测 放到后面去会超时
+        tmp0 = tmp[0];
+        tmp1 = tmp[1];
+        for (int j = m; j >=tmp0 ; --j) {
+            for (int k = n; k>=tmp1; --k) {
+                    dp[j][k] = max(dp[j][k],dp[j-tmp0][k-tmp1]+1);
+            }
+        }
+    }
+    return dp[m][n];
+}
 
 
 int main()
