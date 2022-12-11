d416cb39dbfef3d5113234f0d5f6e611e0764bcd
dfc <yunzhongxicao@sjtu.edu.cn>
2022-12-11 15:03:59 +0800

    offer_14_1 剪绳子

---
+++ OFFER/offer_14_1.cpp
@@ -0,0 +1,22 @@
+/*
+@File  :offer_14_1.cpp
+@Author:dfc
+@Date  :2022/12/11  14:29
+@Desc  : МєЩўзг
+*/
+#include "iostream"
+#include "vector"
+using namespace std;
+
+int cuttingRope(int n)
+{
+    vector<int> dp(n+1,0);
+    dp[1]=1;
+    for (int i = 2; i < n; ++i) {
+        for (int j = 1; j < i; ++j) {
+            dp[i] = max(dp[i], max(dp[i-j] * j,(i-j) * j));
+        }
+    }
+    return dp[n];
+}
+
