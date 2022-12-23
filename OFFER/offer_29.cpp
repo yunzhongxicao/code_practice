296939ea993ea6857576d684b2d13885702a5bb1
dfc <yunzhongxicao@sjtu.edu.cn>
2022-12-23 23:10:15 +0800

    过渡

--- OFFER/offer_29.cpp
+++ OFFER/offer_29.cpp
@@ -17,7 +17,7 @@ vector<int> spiralOrder(vector<vector<int>>& matrix)
     if (n==0)return result;
     int start_x= 0,start_y = 0;
     int  count=0;
-    while (count*2+1 < min(m,n) )
+    while (count< min(m,n)/2 )
     {
         for (;start_x<=n-1-count-1;++start_x) {
             result.push_back(matrix[start_y][start_x]);
