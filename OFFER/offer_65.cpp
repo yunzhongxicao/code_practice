b336f6d6bb49be2bb1e581719c7036c5bdfe3b0f
dfc <yunzhongxicao@sjtu.edu.cn>
2023-01-25 11:02:14 +0800

    offer_65 不用加减乘除做加法

--- OFFER/offer_65.cpp
+++ OFFER/offer_65.cpp
@@ -9,6 +9,15 @@ using namespace std;
 
 int add(int a, int b)
 {
+    while (b!=0)
+    {
+        int temp1 = a^b;
+        //LC c++不支持负值左移，需要强制转换为无符号数
+        int temp2 = (unsigned int )(a&b)<<1;// 求与 再左移 最多32次就为0
+        a = temp1;
+        b = temp2;
+    }
+    return a;
 
 }
 
