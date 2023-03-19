6d324c830bfbacbcef870e18c32b4488d9d32438
dfc <yunzhongxicao@sjtu.edu.cn>
2023-03-19 12:47:57 +0800

    子序列

---
+++ Dynamic/300_second.cpp
@@ -0,0 +1,14 @@
+/*
+@File  :300_second.cpp
+@Author:dfc
+@Date  :2023/3/19  12:44
+@Desc  :
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+int lengthOfLIS(vector<int>& nums)
+{
+    vector<int>dp(nums.size()); // dp[i]表示以nums[i]结尾的递增子序列的长度
+    
+}
