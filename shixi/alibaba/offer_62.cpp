99f340f496c1ac4c03fd7db18641e5df42b3f502
dfc <yunzhongxicao@sjtu.edu.cn>
2023-03-26 19:44:05 +0800

    剑指offer62 约瑟夫环 模拟 超时

--- shixi/alibaba/offer_62.cpp
+++ shixi/alibaba/offer_62.cpp
@@ -5,4 +5,40 @@
 @Desc  :
 */
 
+#include "iostream"
+#include "vector"
 
+using namespace std;
+int lastRemaining(int n, int m)
+{
+    vector<int>next(n);
+    for (int i = 0; i < n; ++i) {
+        next[i] = (i+1) % n;
+    }
+    if (n==1) return 0;
+    if (m==1) return n-1;
+    int count = 2;
+    int cur = 1;
+    int pre = 0;
+    while (next[cur]!=cur)
+    {
+        if (count<m)
+        {
+            pre = cur;
+            cur = next[cur];
+            count++;
+        }
+        else
+        {
+            cur = next[cur];
+            next[pre] = cur;
+            count=1;
+        }
+    }
+    return cur;
+}
+
+int main()
+{
+    cout<<lastRemaining(10,17);
+}
