091e643338a96e590e877a54620deb56b8a748d1
dfc <yunzhongxicao@sjtu.edu.cn>
2023-04-12 16:13:32 +0800

    offer 2 030

--- offer_2/offer_2_30.cpp
+++ offer_2/offer_2_30.cpp
@@ -8,21 +8,27 @@
 #include "vector"
 #include "iostream"
 #include "cstdlib"
+#include "unordered_map"
 using namespace std;
 
 class RandomizedSet {
 public:
     /** Initialize your data structure here. */
-    unordered_set<int> my_set;
+    unordered_map<int,int>my_map;
+    vector<int> nums;
+    int index = 0;
     RandomizedSet() {
         unsigned seed;
+
     }
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        if (my_set.find(val) == my_set.end())
+        if (my_map.find(val) == my_map.end())
         {
-            my_set.emplace(val);
+            my_map[val] = index;
+            nums.push_back(val);
+            index++;
             return true;
         }
         else return false;
@@ -30,9 +36,15 @@ public:
 
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        if (my_set.find(val) != my_set.end())
+        if (my_map.find(val) != my_map.end())
         {
-            my_set.erase(val);
+            int delete_index= my_map[val];
+            int back_index = index-1;
+            my_map[nums[back_index]] = delete_index;
+            swap(nums[delete_index],nums[back_index]);
+            nums.pop_back();
+            my_map.erase(val);
+            index--;
             return true;
         }
         else return false;
@@ -40,8 +52,7 @@ public:
 
     /** Get a random element from the set. */
     int getRandom() {
-        int index = rand() % my_set.size();
-        auto iter = my_set.begin();
-        
+        int num = rand() % my_map.size();
+        return nums[num];
     }
 };
