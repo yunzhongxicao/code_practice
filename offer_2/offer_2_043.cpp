e055bf6fb81d885153502211d07facfe20782aec
dfc <yunzhongxicao@sjtu.edu.cn>
2023-05-09 18:44:59 +0800

    过渡

---
+++ offer_2/offer_2_043.cpp
@@ -0,0 +1,32 @@
+/*
+@File  :offer_2_043.cpp
+@Author:dfc
+@Date  :2023/5/9  11:28
+@Desc  : 往完全二叉树添加节点
+*/
+#include "iostream"
+#include "vector"
+#include "queue"
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x):val(x),left(nullptr),right(nullptr){}
+};
+
+class CBTInserter {
+public:
+    CBTInserter(TreeNode* root) {
+
+    }
+
+    int insert(int v) {
+
+    }
+
+    TreeNode* get_root() {
+
+    }
+};
